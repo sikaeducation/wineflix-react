b77759339da6394cbb8ba3639c2e86b74e1398db
cw-roy <curtis_roy@msn.com>
2023-05-29 11:48:45 -0700

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,5 @@
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-
-const NavigationLink = ({ href, text }) => {
-    return (
-        <li>
-            <a href={href}>{text}</a>
-        </li>
-    );
-};
-
-const Navigation = () => {
-    return (
-        <nav className="navigation">
-            <section className="primary-navigation">
-                <h1>WineFlix</h1>
-                <ul className="navigation-links">
-                    <NavigationLink href="https://developer.mozilla.org" text="Home" />
-                    <NavigationLink href="https://developer.mozilla.org" text="Whites" />
-                    <NavigationLink href="https://developer.mozilla.org" text="Reds" />
-                    <NavigationLink href="https://developer.mozilla.org" text="Blends" />
-                </ul>
-            </section>
-            <section className="secondary-navigation">
-                <span className="user-avatar">
-                    <a href="https://developer.mozilla.org">S</a>
-                </span>
-            </section>
-        </nav>
-    );
-};
-
-const HeroContent = ({ title, subtitle, imageUrl, description }) => {
-    return (
-        <div className="hero-content">
-            <h2>
-                <img alt={title} src={imageUrl} />
-            </h2>
-            <h3>{subtitle}</h3>
-            <p>{description}</p>
-            <ul className="controls">
-                <li>
-                    <button className="primary-action">
-                        <FontAwesomeIcon icon={faPlay} />
-                        <span>Drink</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="secondary-action">
-                        <FontAwesomeIcon icon={faInfoCircle} />
-                        <span>More Info</span>
-                    </button>
-                </li>
-            </ul>
-        </div>
-    );
-};
+import Header from "./Header";
 
 const Wine = ({ imageUrl, isNew, label, isFinished, progress }) => {
     return (
@@ -244,11 +188,12 @@ const App = () => {
 
     return (
         <div className="App">
-            <header className="hero">
+            <Header heroContent={heroContent} />
+            {/* <header className="hero">
                 <img className="hero-image" src="/wine-hero.webp" alt="Wine bottles" />
                 <Navigation />
                 <HeroContent {...heroContent} />
-            </header>
+            </header> */}
             <main>
                 {Object.entries(groupedWines).map(([category, wines]) => (
                     <Category key={category} title={category} wines={wines} />
